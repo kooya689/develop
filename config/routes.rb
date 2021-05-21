639e181d2c13d837f907e5ce2ec4c303f45d18e3
佐藤公弥 <koyasato@satoukouwatarunoMacBook-Pro.local>
2021-05-21 16:36:04 +0900

    tweet_1

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
-  get 'tweets/new'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/create'
-  get 'users/index'
-  get 'users/show'
-  devise_for :users
+  #get 'tweets/new'
+  #get 'tweets/index'
+  #get 'tweets/show'
+  #get 'tweets/create'
+  #get 'users/index'
+  #get 'users/show'
+  #devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  devise_for :users
+  resources  :tweets
+  resources  :users
 end
